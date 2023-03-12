93880c173ee066c0f54e49d519d89116a3e5ad4b
thawkins <tim.thawkins@gmail.com>
2023-03-12 11:54:19 +0800

    get initial k8s scan working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use clap::Parser;
+use k8s_openapi::api::core::v1::Pod;
+use std::{sync::Arc, time::Duration};
+use futures::StreamExt;
+use kube::{
+    Api, Client, ResourceExt,
+    runtime::controller::{Action, Controller}
+};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -18,10 +25,38 @@ struct Args {
     all_namespaces: bool,
 }
 
-fn main() {
+#[derive(thiserror::Error, Debug)]
+pub enum Error {}
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+fn error_policy(_object: Arc<Pod>, _err: &Error, _ctx: Arc<()>) -> Action {
+    Action::requeue(Duration::from_secs(5))
+}
+
+async fn reconcile(obj: Arc<Pod>, ctx: Arc<()>) -> Result<Action> {
+    println!("reconcile request: {}", obj.name_any());
+    Ok(Action::requeue(Duration::from_secs(3600)))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), kube::Error>  {
     let args = Args::parse();
     println!("context {}", args.context);
     println!("namespacet {}", args.output);
     println!("output {}", args.output);
     println!("all_namespaces {}", args.all_namespaces);
+
+    let client = Client::try_default().await?;
+    let pods = Api::<Pod>::all(client);
+
+    Controller::new(pods.clone(), Default::default())
+        .run(reconcile, error_policy, Arc::new(()))
+        .for_each(|_| futures::future::ready(()))
+        .await;
+
+    Ok(())
+
+
+
+
 }
